d106e81aa4b01608c2fe547572f17d86c109d0c0
minhlong149 <minhlong9514@gmail.com>
2023-07-28 18:44:22 +0700

    Update cart styling

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -10,14 +10,12 @@ export default function Cart({
   increaseQuantity,
   decreaseQuantity,
 }) {
-  if (cart.length === 0) {
-    return <h1>Your cart is empty</h1>;
-  }
-
-  const totalPrice = cart.reduce(
-    (totalPrice, product) => totalPrice + product.price * product.quantity,
-    0
-  );
+  const totalPrice = Math.round(
+    cart.reduce(
+      (totalPrice, product) => totalPrice + product.price * product.quantity,
+      0
+    ) * 100
+  ) / 100;
 
   return (
     <section className="cart-container">
@@ -26,17 +24,21 @@ export default function Cart({
         <h1 className="header-title">Your Cart</h1>
         <span className="header-subtitle">{totalPrice}</span>
       </section>
-      <section className="cart-list">
-        {cart.map((product) => (
-          <CartItem
-            key={product.id}
-            product={product}
-            removeFromCart={removeFromCart}
-            increaseQuantity={increaseQuantity}
-            decreaseQuantity={decreaseQuantity}
-          />
-        ))}
-      </section>
+      {cart.length > 0 ? (
+        <section className="cart-list">
+          {cart.map((product) => (
+            <CartItem
+              key={product.id}
+              product={product}
+              removeFromCart={removeFromCart}
+              increaseQuantity={increaseQuantity}
+              decreaseQuantity={decreaseQuantity}
+            />
+          ))}
+        </section>
+      ) : (
+        <p className="cart-empty">Your cart is empty</p>
+      )}
     </section>
   );
 }
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,25 +1,53 @@
+import minusIcon from "../assets/minus.png";
+import plusIcon from "../assets/plus.png";
+import trashIcon from "../assets/trash.png";
+
 export default function CartItem({
   product,
   removeFromCart,
   increaseQuantity,
   decreaseQuantity,
 }) {
-  const { name, image, description, price, color, quantity } = product;
+  const { name, image, price, color, quantity } = product;
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>{description}</p>
-      <p>{price}</p>
-      <img src={image} alt={name} />
-      <p>{quantity}</p>
-      <button onClick={() => removeFromCart(product)}>Remove from cart</button>
+    <article className="cart-item">
+      <img src={image} className="cart-item-image" alt={name} />
+      <h2 className="cart-item-name">{name}</h2>
+      <p className="cart-item-price">{price}</p>
+      <div className="cart-item-quantity-wrapper">
+        <button
+          onClick={() => decreaseQuantity(product)}
+          disabled={quantity === 1}
+          className="cart-item-icon-wrapper cart-item-icon-wrapper__accent"
+        >
+          <img
+            src={minusIcon}
+            className="cart-item-icon"
+            alt="decrease-icon-button"
+          />
+        </button>
+        <span className="cart-item-quantity">{quantity}</span>
+        <button
+          onClick={() => increaseQuantity(product)}
+          className="cart-item-icon-wrapper cart-item-icon-wrapper__accent"
+        >
+          <img
+            src={plusIcon}
+            className="cart-item-icon"
+            alt="increase-icon-button"
+          />
+        </button>
+      </div>
       <button
-        onClick={() => decreaseQuantity(product)}
-        disabled={quantity === 1}
+        className="cart-item-remove cart-item-icon-wrapper cart-item-icon-wrapper__primary"
+        onClick={() => removeFromCart(product)}
       >
-        -
+        <img
+          src={trashIcon}
+          className="cart-item-icon"
+          alt="remove-icon-button"
+        />
       </button>
-      <button onClick={() => increaseQuantity(product)}>+</button>
-    </div>
+    </article>
   );
 }
