e0675f6a9d3d12e9ba084f59239909e55cbf61f5
minhlong149 <minhlong9514@gmail.com>
2023-07-28 22:00:43 +0700

    Fetch data from server

--- src/services/products.js
+++ src/services/products.js
@@ -1,5 +1,19 @@
 import products from "../data/shoes.json";
 
+const BASE_URL = "./api/v1/products";
+
 export async function getAllProducts() {
-  return products.shoes;
+  try {
+    const response = await fetch(BASE_URL);
+    if (!response.ok) {
+      throw new Error();
+    }
+    const data = await response.json();
+    return data.shoes;
+  } catch (error) {
+    console.log(
+      "An error occurred while fetching the products. Using mock data instead."
+    );
+    return products.shoes;
+  }
 }
