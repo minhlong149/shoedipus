9d4500ec5cf66ae2f9cc400e1acf58bda209e3ab
minhlong149 <minhlong9514@gmail.com>
2023-07-28 17:49:29 +0700

    Update products styling

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 
+import checkIcon from "../assets/check.png";
+
 export default function Product({ product, addToCart }) {
   const { name, image, description, price, color, inCart } = product;
   return (
-    <div>
-      <h1>{name}</h1>
-      <p>{description}</p>
-      <p>{price}</p>
-      <img src={image} alt={name} />
+    <article className="product">
+      <div className="product-image-wrapper" style={{ backgroundColor: color }}>
+        <img src={image} className="product-image" alt={name} />
+      </div>
+      <h1 className="product-title">{name}</h1>
+      <p className="product-description">{description}</p>
+      <p className="product-price">{price}</p>
       {inCart ? (
-        <button disabled>Already in cart</button>
+        <img src={checkIcon} className="product-checkout" alt="check-icon" />
       ) : (
-        <button onClick={() => addToCart(product)}>Add to cart</button>
+        <button onClick={() => addToCart(product)} className="product-checkout">
+          Add to cart
+        </button>
       )}
-    </div>
+    </article>
   );
 }
